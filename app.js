01340929b0048eaa6e8e03276cdb85ad02f16103
aselnigmatullina <nigmatullina.asel2016@yandex.ru>
2019-11-14 23:25:15 +0300

    a completely different solution

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 console.log('worked');
 
+let nextId = 1;
 const rootEl = document.getElementById('root');
-
-const posts = [];
-const addFormEl = document.createElement('form');
-addFormEl.className = 'form-inline mb-2';
-addFormEl.innerHTML = `
+const formEl = document.createElement('form');
+formEl.className = 'form-inline mb-2';
+formEl.innerHTML = `
     <div class="form-group">
         <input class="form-control" data-id="link">
     </div>
@@ -17,56 +16,50 @@ addFormEl.innerHTML = `
     </select>
     <button class="btn btn-primary">Ok</button>
 `;
-const linkEl = addFormEl.querySelector('[data-id=link]');
-const typeEl = addFormEl.querySelector('[data-id=type]');
+const linkEl = formEl.querySelector('[data-id=link]');
+const typeEl = formEl.querySelector('[data-id=type]');
 
-addFormEl.addEventListener('submit', function (ev) {
+formEl.addEventListener('submit', ev => {
     ev.preventDefault();
     const value = linkEl.value;
     const type = typeEl.value; 
-    console.log(type);
-    posts.push({
+
+    const post = {
+        id:nextId++,
         value,
         type,
-        likes: 0,
-        favorite: false,
-        selected: false,
-    }); 
-    console.log(posts);
+        likes: 0,       
+    } 
+    addPost(postsEl, post);
     linkEl.value = '';
     typeEl.value = 'regular';
-    rebuildList(postsEl, posts);
+    linkEl.focus();
+    
 });
-rootEl.appendChild(addFormEl);
+rootEl.appendChild(formEl);
 
 const postsEl = document.createElement('div');
 rootEl.appendChild(postsEl);
 
-function rebuildList(containerEl, items) {
-    containerEl.innerHTML = '';
-    for (const item of items) {
+function addPost(containerEl, item) {
         const postEl = document.createElement('div');
         postEl.className = 'card mb-2';
         if (item.type === 'regular') {
             postEl.innerHTML = `
                 <div class="card-body">
                     <div class="card-text">${item.value}</div>
-                    <button class="btn">♡ ${item.likes}</button>
+                    <button class="btn" data-id="likes">♡ ${item.likes}</button>
                     <button class="btn btn-primary" data-action="like">like</button>
-                    <button class="btn btn-danger" data-action="dislike">dislike</button>
-                    <button class="btn btn-danger" data-action="delete">delete</button>
-                    <button class="btn btn-primary" data-action="toggle-favorite">Выбрать</button>    
+                    <button class="btn btn-danger" data-action="dislike">dislike</button>   
                 </div>
             `;
         } else if (item.type === 'image') {
             postEl.innerHTML = `
-                <img src="${item.value}" class="card-img-top">
+                <img src="${item.value}" class="card-img-top" width="200" height="200">
                 <div class="card-body">
-                    <button class="btn">♡ ${item.likes}</button>
+                    <button class="btn" data-id="likes"> ♡ ${item.likes}</button>                    
                     <button class="btn btn-primary" data-action="like">like</button>
                     <button class="btn btn-danger" data-action="dislike">dislike</button>
-                    <button class="btn btn-danger" data-action="delete">delete</button>
-                    <button class="btn btn-primary" data-action="toggle-favorite">Выбрать</button>
                 </div>
             `;
         } else if(item.type ==='video'){
@@ -76,11 +69,9 @@ function rebuildList(containerEl, items) {
                 <video src = "${item.value}" class = "embed-responsive-item" controls>
                 </div>
                 <div class = "card-body">
-                <button class="btn">♡ ${item.likes}</button>
+                <button class="btn" data-id="likes"> ♡ ${item.likes}</button>
                 <button class="btn btn-primary" data-action="like">like</button>
                 <button class="btn btn-danger" data-action="dislike">dislike</button>
-                <button class="btn btn-danger" data-action="delete">delete</button>
-                <button class="btn btn-primary" data-action="toggle-favorite">Выбрать</button>   
                 </div>
                 </div>
             `;
@@ -91,41 +82,45 @@ function rebuildList(containerEl, items) {
                 <audio src = "${item.value}"  class = "embed-responsive-item" controls>
             </div>
             <div class = "card-body"
-            <button class="btn">♡ ${item.likes}</button>
+            <button class="btn" data-id="likes">♡ ${item.likes}</button>
             <button class="btn btn-primary" data-action="like">like</button>
             <button class="btn btn-danger" data-action="dislike">dislike</button>
-            <button class="btn btn-danger" data-action="delete">delete</button>
-            <button class="btn btn-primary" data-action="toggle-favorite">Выбрать</button>
             </div>
             </div>
             `;
-        }
-
-        postEl.addEventListener('click', function (ev) {
-            if (ev.target.dataset.action === 'delete') {
-                ev.currentTarget.parentElement.removeChild(ev.currentTarget);
-                return;
-            }
-
-            if (ev.target.dataset.action === 'toggle-favorite') {
-                ev.currentTarget.classList.toggle('message_favorite');
-                return; 
-            }
-            if(ev.target.dataset.action ==='like'){      
-                item.likes++;
-                items.sort((a,b) => b.likes-a.likes)
-                rebuildList(containerEl, items);
-                }
-
-            if(ev.target.dataset.action ==='dislike'){
-                item.likes < 1 ? 0 : item.likes--;
-                items.sort((a,b) => b.likes - a.likes)
-                rebuildList(containerEl, items)
-                }
-            ev.currentTarget.classList.toggle('message_selected');
-        });    
-        containerEl.appendChild(postEl);              
+        };
+        postEl.item=item;
+        postEl.querySelector('[data-action=like]').addEventListener('click', ev =>{
+            item.likes++;
+            syncPost(postEl);
+        });
+        postEl.querySelector('[data-action=dislike]').addEventListener('click',ev =>{
+            item.likes--;
+            syncPost(postEl);
+        });
+        containerEl.appendChild(postEl);
     }
-}
+
+    function syncPost(itemEl){
+        itemEl.querySelector('[data-id=likes]').textContent = itemEl.item.likes;
+        const parentEl = itemEl.parentElement;
+        const childrenEl = Array.from(parentEl.children);
+        childrenEl.sort((a,b) =>{
+            return -(a.item.likes - b.item.likes);
+        });
+        const itemIndex = childrenEl.indexOf(itemEl);
+        if(itemIndex === 0){
+            parentEl.insertBefore(itemEl,parentEl.firstElementChild);
+            return;
+        }
+        parentEl.insertBefore(itemEl,childrenEl[itemIndex+1]);
+    }
+        
+          
+
+        
+           
+   
+ 
     
         
